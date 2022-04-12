11c50b489833bb8fed906f5c83a0d87459f8c218
Iury1978 <advent777@yandex.ru>
2022-04-12 20:07:22 +0300

    created styles for section book and js for button search

--- src/js/index.js
+++ src/js/index.js
@@ -4,7 +4,9 @@ const library = document.querySelector('.library');
 const book = document.querySelector('.book');
 const add = document.querySelector('.add');
 const addBtns = document.querySelectorAll('.header__btn-add, .library__add-btn');
-const  backBtn = document.querySelector('.header__btn_back');
+const  backBtn = document.querySelectorAll('.header__btn_back');
+const btnSearch = document.querySelectorAll('.header__btn_search');
+const search = document.querySelector('.search');
 
 const router = new Navigo("/", {hash: true,});
 
@@ -37,6 +39,28 @@ addBtns.forEach(btn => {
   })
 });
 
-backBtn.addEventListener('click', () =>{
-  router.navigate('/');
-})
+
+backBtn.forEach(btn => {
+  btn.addEventListener('click', () => {
+    router.navigate('/');
+  })
+});
+// с помощью деструктуризации достаем из евента объект тргет
+const closeSearch = ({target}) => {
+  // tckb в таргете будет класс search, то ретурн,
+  // если нет- закрываем всплывающее окно?  удаляя класс search_active
+
+  if (target.closest('.search, .header__btn_search')) {
+    return;
+  }
+  search.classList.remove('search_active');
+  document.body.removeEventListener('click', closeSearch);
+};
+
+
+btnSearch.forEach(btn => {
+  btn.addEventListener('click', () => {
+    search.classList.add('search_active');
+    document.body.addEventListener('click', closeSearch);
+  });
+});
