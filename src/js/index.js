eb013a73e230d96e3edaad3fa32d3073c28fefd0
Iury1978 <advent777@yandex.ru>
2022-04-12 13:53:32 +0300

    created all in ccs before adaptive

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,42 @@
-// import {sum} from './modules/test.js';
+import Navigo from "navigo";
 
-// document.write(sum(3,6))
+const library = document.querySelector('.library');
+const book = document.querySelector('.book');
+const add = document.querySelector('.add');
+const addBtns = document.querySelectorAll('.header__btn-add, .library__add-btn');
+const  backBtn = document.querySelector('.header__btn_back');
+
+const router = new Navigo("/", {hash: true,});
+
+// создаем обработчики, которые будут запускаться в зависимости от адресной строки
+
+const closeAllPage = () => {
+  library.classList.add('hidden');
+  book.classList.add('hidden');
+  add.classList.add('hidden');
+}
+
+router.on({
+  '/': () => {
+    closeAllPage();
+      library.classList.remove("hidden");
+  },
+  'book': () => {
+    closeAllPage();
+      book.classList.remove("hidden");
+  },
+  'add' : () => {
+    closeAllPage();
+      add.classList.remove("hidden");
+  }
+}).resolve();
+
+addBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    router.navigate('add');
+  })
+});
+
+backBtn.addEventListener('click', () =>{
+  router.navigate('/');
+})
